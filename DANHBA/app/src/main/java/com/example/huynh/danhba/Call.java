4c7719a094bc6baab55d00498875b59d3a210139
trilong0610 <68650596+trilong0610@users.noreply.github.com>
2020-08-05 22:33:53 +0700

    Đã thêm layout bàn phím. Chưa làm intent gọi active bàn phím

--- DANHBA/app/src/main/java/com/example/huynh/danhba/Call.java
+++ DANHBA/app/src/main/java/com/example/huynh/danhba/Call.java
@@ -1,24 +1,28 @@
 package com.example.huynh.danhba;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
 
-
-public class Call extends Fragment {
+public class Call extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
 
-    }
+        EditText editText = (EditText) findViewById(R.id.phone_number);
+        Keyboard keyboard = (Keyboard) findViewById(R.id.keyboard);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_call, container, false);
+        // prevent system keyboard from appearing when EditText is tapped
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        // pass the InputConnection from the EditText to the keyboard
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
     }
 }
